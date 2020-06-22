341156ed540c1c73085c7beb9bbc2a72738d8662
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-23 00:56:11 +0300

    Split phase refactoring applied
          - Board update extracted to a function
          - Creation of chess notation extracted to a function
          -  Logic of eating opponent's chess piece extracted to a function

--- src/chess/chess_board.py
+++ src/chess/chess_board.py
@@ -64,41 +64,12 @@ class ChessBoard:
 
     def move_chess_piece(self, chess_move: ChessMove):      
         # buraya gelindiginde yapilan bir move'un valid oldugunu dusunuyoruz
-        chess_piece = chess_move.chess_piece()
-        destination = chess_move.destination()
+        chess_notation = self.__create_chess_notation(chess_move)
 
-        y_target = destination['y']
-        x_target = destination['x']
-
-        x = chess_piece.x()
-        y = chess_piece.y()        
-
-        print('x: ' + str(x))
-        print('y: ' + str(y))
-
-        source = self.__matrix_to_chess_mapper[y][x]
-        destination = self.__matrix_to_chess_mapper[y_target][x_target]
-
-        move = chess.Move.from_uci(source + destination)
+        move = chess.Move.from_uci(chess_notation)
         self.__board_engine.push(move)
 
-        self.__board[y][x] = 0
-        
-        ## TODO: Eger burada bir tas varsa o da silinmeli arrayler var ya oradan silinmeli
-        target_piece = self.__board[y_target][x_target]
-
-        if target_piece != 0:
-            print('Target chess piece: ' + target_piece.get_id())
-            print('In black pieces: ' + str(target_piece in self.__black_pieces))
-            print('In white pieces: ' + str(target_piece in self.__white_pieces))
-            if target_piece.color() == 'white':
-                self.__white_pieces.remove(target_piece)
-            else:
-                self.__black_pieces.remove(target_piece)            
-        
-        self.__board[y_target][x_target] = chess_piece
-
-        chess_piece.set_position(y_target, x_target)
+        self.__update_chess_board(chess_move)
 
         ##TODO: source'un yani piece'in bulundugu yerin board karsiligini al mapper ile
         ##TODO: destination'ın board karsiligini al mapper ile 
@@ -128,8 +99,45 @@ class ChessBoard:
         # TODO: tabii bir de sah yapildigi zaman oncesinde oyun bitti mi diye bir kontrol yapmak gerekiyor cunku oyun bittiyse hamle yapamamali gerci bunu while'daki kosul ile sagliyoruz sanirsam
 
         # TODO: sah durumu vs varsa board farklı bir sekilde renderlanmali sanki
+        # TODO: piyon en sona geldiği zaman kural olarak baska bir tasa evrilebiliyordu onu da eklemek gerek
 
         return
+
+    def __create_chess_notation(self, chess_move):
+        chess_piece = chess_move.chess_piece()
+        destination = chess_move.destination()
+
+        y_target = destination['y']
+        x_target = destination['x']
+
+        x = chess_piece.x()
+        y = chess_piece.y()        
+
+        return self.__matrix_to_chess_mapper[y][x] + self.__matrix_to_chess_mapper[y_target][x_target]
+
+    def __update_chess_board(self, chess_move):
+        chess_piece = chess_move.chess_piece()
+        source_y, source_x = chess_piece.y(), chess_piece.x()
+
+        destination = chess_move.destination()
+        target_y, target_x = destination['y'], destination['x']
+
+        self.__board[source_y][source_x] = 0        
+        target_piece = self.__board[target_y][target_x]
+
+        if target_piece != 0:
+            self.__eat_opponents_chess_piece(target_piece)
+        
+        self.__board[target_y][target_x] = chess_piece
+        chess_piece.set_position(target_y, target_x)
+
+    def __eat_opponents_chess_piece(self, target_piece):
+            print('Chess piece: ' + target_piece.get_id() + ' has been eaten.')
+            
+            if target_piece.color() == 'white':
+                self.__white_pieces.remove(target_piece)
+            else:
+                self.__black_pieces.remove(target_piece)            
 
     def game_not_finished(self):
         ## TODO: should be implemented
